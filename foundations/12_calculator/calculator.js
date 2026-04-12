85d66c4afaff89814065975498c8bf6d0fd78924
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-12 21:56:25 +0100

    feat: multiply all numbers in an array

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -10,7 +10,9 @@ const sum = function (arr) {
   return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((acc, num) => acc * num, 1);
+};
 
 const power = function () {};
 
