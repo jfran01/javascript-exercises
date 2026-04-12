25ffc3b9d98acb818e6938d885ebde64cb09384f
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-12 21:38:35 +0100

    Commit all changes

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, remove) {
+  let index = array.indexOf(remove);
+  let removedArray = array.splice(index, 1);
+  return removedArray;
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let farenheit = temp * (9 / 5) + 32;
+  farenheit = Math.round(farenheit * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
