e1b3e5dca2bd6e2abef7abaf1e069b0667a23451
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-12 22:12:00 +0100

    feat: find the factorial of a number

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -18,7 +18,13 @@ const power = function (a, b) {
   return a ** b;
 };
 
-const factorial = function () {};
+const factorial = function (num) {
+  let init = 1;
+  for (let i = 1; i <= num; i++) {
+    init *= i;
+  }
+  return init;
+};
 
 // Do not edit below this line
 module.exports = {
