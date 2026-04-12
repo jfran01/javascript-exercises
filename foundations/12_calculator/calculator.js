e8145e9fc434a36beeb2a50aa58def1e14158ee8
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-12 21:58:47 +0100

    feat: return the result of raising a to b

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -14,7 +14,9 @@ const multiply = function (arr) {
   return arr.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
 const factorial = function () {};
 
