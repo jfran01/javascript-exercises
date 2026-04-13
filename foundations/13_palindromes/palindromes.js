5e762943a33dc286674717b8b074f5920eaaba39
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-13 12:34:49 +0100

    feat: check if a given string is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let reverse = cleanString.split("").reverse().join("");
+
+  if (cleanString == reverse) return true;
+  else return false;
 };
 
 // Do not edit below this line
