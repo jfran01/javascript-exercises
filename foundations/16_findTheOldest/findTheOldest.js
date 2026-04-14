42d898d8c16070c02007d61294261c514bee411e
Juliette <211356692+jfran01@users.noreply.github.com>
2026-04-14 10:19:18 +0100

    feat: find the oldest person from an array

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    return new Date().getFullYear() - birth;
+  } else return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    if (
+      getAge(current.yearOfBirth, current.yearOfDeath) >
+      getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    )
+      return current;
+    return oldest;
+  });
 };
 
 // Do not edit below this line
